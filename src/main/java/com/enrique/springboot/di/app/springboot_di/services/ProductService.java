0a343c2de24de230be520d7300ae36309a9e420e
Enrique <munoze03@gmail.com>
2025-07-21 16:01:29 +0200

    Escribiendo la clase Service

---
+++ src/main/java/com/enrique/springboot/di/app/springboot_di/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.enrique.springboot.di.app.springboot_di.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.enrique.springboot.di.app.springboot_di.models.Product;
+import com.enrique.springboot.di.app.springboot_di.repositories.ProductRepository;
+
+public class ProductService {
+
+    private ProductRepository repository = new ProductRepository();
+
+    public List<Product> findAll(){
+
+        return repository.findAll().stream().map(p -> {
+            Double priceImp = p.getPrice() * 1.21d;
+            p.setPrice(priceImp.longValue());
+            return p;
+        }).collect(Collectors.toList());
+    }
+
+    public Product findById(Long id){
+
+        return repository.findById(id);
+    }
+}
